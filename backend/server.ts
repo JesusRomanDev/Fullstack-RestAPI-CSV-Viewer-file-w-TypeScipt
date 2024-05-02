9f310560d26a7bc260c8914ffbbf6d31afd393df
Jesus Roman <jesus_fime@outlook.es>
2024-05-02 08:21:51 -0600

    Adding get endpoint

--- backend/server.ts
+++ backend/server.ts
@@ -16,6 +16,9 @@ const port = process.env.PORT || 4000;
 const storage = multer.memoryStorage();
 const upload = multer({storage: storage});
 
+//Por el momento vamos a guardarlo en memoria
+let userData: Array<Record<string, string>> = [];
+
 
 
 app.use(cors());
@@ -32,25 +35,38 @@ app.post('api/files', upload.single('file'), async (req, res)=> {
         return res.status(500).json({message: 'File must be CSV'});
     }
     //4.Transform the File(Buffer) to string
+    let json: Array<Record<string, string>> = [];
     try {
         //Nota: Los datos no viajan como cadenas de texto en si, viajan en dato binario, entonces para poder transformarlo
         //a un string tenemos que hacer lo de abajo.
-        Buffer.from(file.buffer).toString('utf-8');
+        const rawCsv = Buffer.from(file.buffer).toString('utf-8');
+        //5. Transform string (csv) to JSON osea transformando esa cadena de texto a un json
+        json = csvToJson.csvStringToJson(rawCsv);
     } catch (error) {
-        
+        return res.status(500).json({message: 'Error parsing the file'})
     }
-    //5. Transform string to CSV
     //6.Save the JSON to db (or memory)
+    userData = json; //<-----asignandolo en memoria
     //7.Return 200 with the message and JSON
-    return res.send(200).json({data: [], message: 'File uploaded successfully'})
+    return res.send(200).json({data: userData, message: 'File uploaded successfully'})
 })
 
 app.get('api/users', async (req,res) => {
     //1.Extract the query param 'q' from the request
+    const {q} = req.query;
     //2.Validate that we have the query param
+    if(!q){
+        return res.status(500).json({message: 'Query param "q" is required'})
+    }
     //3.Filter the data from the db with the query param
+    //Haciendolo insensible a mayusculas o minusculas, aqui lo abajo lo hacemos todo minusculas
+    const search = q.toString().toLowerCase();
+
+    const filteredData = userData.filter( row => {
+        return Object.values(row).some(value => value.toLowerCase().includes(search));
+    });
     //4.Return 200 with the filtered data
-    return res.send(200).json({data: []})
+    return res.send(200).json({data: filteredData})
 })
 
 app.listen(port, () => {
