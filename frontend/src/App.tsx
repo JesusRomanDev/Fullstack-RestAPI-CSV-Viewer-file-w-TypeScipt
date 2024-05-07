0174cb9901d5b1404451d4a34ccc6d59fc138118
Jesus Roman <jesus_fime@outlook.es>
2024-05-07 17:33:02 -0600

    Adding status object and 2 handle functions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
+//Haciendo que nuestra UI por asi decirlo, tenga diferentes pasos
+//Osea que nuestra aplicacion tiene diferentes estados que en los que se encuentre que visualmente va a cambiar
+const APP_STATUS = {
+  IDLE: 'idle', //estado cuando acabamos de entrar
+  ERROR: 'error', //cuando hay un error
+  READY_UPLOAD: 'ready_upload', //al elegir el archivo
+  UPLOADING: 'uploading', //mientras se sube
+  READY_USAGE: 'ready_usage', //esto es cuando ya podamos hacer las busquedas, despues de subir
+} as const
+
+type AppStatusType = typeof APP_STATUS[keyof typeof APP_STATUS]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [appStatus, setAppStatus] = useState<AppStatusType>(APP_STATUS.IDLE);
+  const [file, setFile] = useState<File | null>(null)
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const [file] = event.target.files ?? [];
+    console.log(file); 
+    if(file){
+      setFile(file);
+      setAppStatus(APP_STATUS.READY_UPLOAD);
+    }
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) =>{
+    event.preventDefault();
+    console.log('TODO');
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h4>Upload CSV + Search</h4>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            onChange={handleInputChange}
+            type="file"
+            accept='.csv'
+            name="file"
+            id="" />
+        </label>
+        <button>Subir Archivo</button>
+      </form>
     </>
   )
 }
